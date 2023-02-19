4d45beaa2947b0dad630fe170c1ca0ac197c210c
Ramon_Trigo <ramtrigo@gmail.com>
2023-02-19 19:25:18 -0300

    eventos do formulario

---
+++ mod1/js/aula8.js
@@ -0,0 +1,63 @@
+$(function () {
+
+    $('.place')
+        .each(function () {
+            var place = $(this).attr('title');
+            var input = $(this);
+
+            input
+                .val(place)
+                .css('color', '#ccc')
+                .focusin(function () {
+                    input
+                        .css('color', '#000');
+                    if (input.val() == place) {
+                        input.val('');
+                    }
+                }).focusout(function () {
+                    if (input.val() == '') {
+                        input.css('color', '#ccc');
+                        input.val(place);
+                    }
+                });
+        });
+
+
+    var ex = $('.ex1');
+
+    /*
+$('.ev1')
+    .focus(function () {
+        ex.text($(this)
+            .attr('title'));
+    }).keyup(function () {
+        if ($(this)
+            .val() == 'estudo') {
+            $('.ev2').focus();
+        }
+    });
+
+
+$('.ev1')
+    .blur(function () {
+        ex.text("Saida do campo: " + $(this).attr('name'));
+    });
+
+    */
+    $('.ev1')
+        .change(function () {
+            ex.text("Campo Alterado  " + $(this).val());
+        });
+
+    $('.selecionar')
+        .click(function () {
+            $('.ev3')
+                .select();
+                $('form')
+                .submit(function()  {
+                    return false;
+                })
+        });
+
+
+});
