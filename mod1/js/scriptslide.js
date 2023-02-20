3630b12a470a68bc571270c3650e18008dcf4a29
Ramon_Trigo <ramtrigo@gmail.com>
2023-02-19 22:20:27 -0300

    carrossel

---
+++ mod1/js/scriptslide.js
@@ -0,0 +1,20 @@
+$(function () {
+
+    $('#slide img:eq(0)').addClass('ativo').show();
+    var texto = $(".ativo").attr("alt");
+    $('#slide').prepend("<p>" + texto + "</p>");
+
+    setInterval(slide, 3000);
+
+    function slide() {
+        if ($('.ativo').next().size()) {
+
+            $('.ativo').fadeOut().removeClass("ativo").next().fadeIn().addClass("ativo");
+        } else {
+            $('.ativo').fadeOut().removeClass("ativo");
+            $('#slide img:eq(0)').fadeIn().addClass("ativo");
+
+        }
+    }
+
+});
