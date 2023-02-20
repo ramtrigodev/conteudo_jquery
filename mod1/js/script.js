3a0e5dfbf5e3243cbb6dc3c0243152191857ceda
Ramon_Trigo <ramtrigo@gmail.com>
2023-02-19 21:45:54 -0300

    criando_menu_fixo

---
+++ mod1/js/script.js
@@ -0,0 +1,13 @@
+$(function(){
+
+     var nav= $('.menu');
+     $(window).scroll(function()  {
+       
+        if($(this).scrollTop()> 173){
+            nav.addClass("menu-fixo");
+        }else{
+            nav.removeClass("menu-fixo");
+        }
+
+     })
+});
